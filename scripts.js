92d0b957b7c30d9e739567ebf74a1971a7aa661b
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-02-04 14:49:21 +0100

    Add js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
+const btn = document.querySelector('button');
+const output = document.querySelector('output');
 
+function randomNumbers(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function showNumber() {
+    const numbers = [];
+    let random;
+    
+    for(let i = 0; i < 6; i++) {
+        random = randomNumbers(1,49);
+        
+        while(numbers.indexOf(random) !== -1) {
+            random = randomNumbers(1,49);
+            console.log('powtórzyła się ' + random)
+        }
+        
+        numbers.push(random)
+    }
+    
+    output.value = numbers.join(', ')
+}
+
+btn.addEventListener('click', showNumber);
